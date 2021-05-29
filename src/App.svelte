c578c7ab5e8723a472d2fe6c6a6c519957566481
cch099 <50557026+cch099@users.noreply.github.com>
2021-05-28 21:15:45 -0600

    version 1.0 working

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	let idx = 0;
+	let len = 3;
+	let isAscending = false;
+	import Card from "./Card.svelte";
+	import Button from "./Button.svelte";
+	import { flip } from "svelte/animate";
+	let arr;
+	$: {
+	  arr = [...Array(len).keys()].map(x => idx + x);
+	  if (isAscending) arr.reverse();
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+  </script>
+  
+  <main class="App">
+	<h1>
+	  Universe of
+	  <em>Star Wars</em>
+	</h1>
+	<div class="controls">
+	  <label>
+		Show
+		<input type="number" bind:value={len} min="0" />
+	  </label>
+	  <div>
+		<input id="ascending" type="checkbox" bind:checked={isAscending} />
+		<label for="ascending">Ascending?</label>
+	  </div>
+	  <Button on:click={() => idx++}>{idx}</Button>
+	</div>
+	{#each arr as id (id)}
+	  <div animate:flip={{ duration: 500 }}>
+		<Card idx={id} />
+	  </div>
+	{:else}
+	  <img
+		alt="snap"
+		src="https://i.insider.com/5ae3327e19ee8657008b45f6?width=750&format=jpeg&auto=webp"
+	  />
+	{/each}
+  </main>
